fb379b7bb1c14aa9aa6d8161d68ff999734afb7a
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-19 16:12:46 +0530

    Refactor arithmetic operation scripts: move to 'Arithmetic Operators' directory

---
+++ 19-05-2025/Comparison Operators/comparisonOperators.py
@@ -0,0 +1,4 @@
+num1 = int (input("enter first number "))
+num2 = int (input("enter second number "))
+result = num1 < num2        # number 1 is less than (<) number 2 will give True.
+print (result)
