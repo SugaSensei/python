cb0561b5bd1acf2590b7019c000153ee64f5ecf8
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-19 16:40:50 +0530

    Add logicalOperator1.py for temperature evaluation based on user input

---
+++ 19-05-2025/Logical Operators/logicalOperator1.py
@@ -0,0 +1,7 @@
+print ("this program is to check weather good or bad")
+temprature = int (input("input the tempature value "))
+
+if temprature > 0 and temprature < 30:
+    print ("The temprature is good")
+else:
+    print ("The temprature is bad")
