832a4225a738e5c6cfad9fd239547eb8886442a4
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-19 17:31:27 +0530

    Add areaOfRectangle.py for calculating the area of a rectangle

---
+++ 19-05-2025/Arithmetic Operators/areaOfRectangle.py
@@ -0,0 +1,8 @@
+import math
+
+print("this program is for calculating the area of rectangle")
+print("-----------------------------------------------------")
+length = float(input("enter the length "))
+width = float(input("enter the width "))
+area = width * length
+print("-----------------------------------------------------")
