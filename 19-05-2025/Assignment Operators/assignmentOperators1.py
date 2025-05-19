45b9494acf24b0d6cdb163b3521a097ab1d1d51e
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-19 16:52:43 +0530

    Add assignmentOperators1.py for user input of assignment operator values

---
+++ 19-05-2025/Assignment Operators/assignmentOperators1.py
@@ -0,0 +1,4 @@
+print("assignment operator please provide the values for x and y")
+x = int (input("enter value for x: "))
+y = int (input("enter value for y: "))
+print("value of x is", x, "and value of y is", y)
