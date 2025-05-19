73b9c94790559f07297f4b59cce956d5b1b3fa16
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-19 18:26:08 +0530

    Add positiveNegativeZero.py for checking if a number is positive, negative, or zero

---
+++ 19-05-2025/Arithmetic Operators/positiveNegativeZero.py
@@ -0,0 +1,9 @@
+print("here we check number is positive (+) or is it negetive (-)")
+number = int(input("enter the number : "))
+
+if number > 0:
+    print(number, "is positive (+)")
+elif number == 0:
+    print(number, "is nutral")
+else:
+    print(number, "is negetive (-)")
