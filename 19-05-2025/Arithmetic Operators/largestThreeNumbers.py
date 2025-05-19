20baa1eeeb03571dc1bbddfec90c1addc3fa4b5d
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-19 18:37:57 +0530

    Add functionality to find the smallest number among three inputs

--- 19-05-2025/Arithmetic Operators/largestThreeNumbers.py
+++ 19-05-2025/Arithmetic Operators/largestThreeNumbers.py
@@ -2,6 +2,7 @@ a = int(input("Enter first: "))
 b = int(input("Enter second: "))
 c = int(input("Enter third: "))
 
+#highest
 if a >= b and a >= c:
     print("largest number is :", a)
 elif b >= a and b >= c:
@@ -9,4 +10,13 @@ elif b >= a and b >= c:
 else:
     print("largest number is :", c)
     
+
+#smallest
+if a <= b and a <= c:
+    print("smallet number is :", a)
+elif b <= a and b <= c:
+    print("smallet number is :", b)
+else:
+    print("smallet number is :", c)
+
     
