9c33bb7c2a429654a8fe74258a9536d3afac9a65
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-19 16:16:21 +0530

    Refactor comparison operators script: separate results for less than and greater than comparisons

--- 19-05-2025/Comparison Operators/comparisonOperators.py
+++ 19-05-2025/Comparison Operators/comparisonOperators.py
@@ -1,4 +1,9 @@
 num1 = int (input("enter first number "))
 num2 = int (input("enter second number "))
-result = num1 < num2        # number 1 is less than (<) number 2 will give True.
-print (result)
+result1 = num1 < num2        # number 1 is less than (<) number 2.
+print (result1)
+
+num3 = int (input("enter first number "))
+num4 = int (input("enter second number "))
+result2 = num3 > num4        # number 1 is greater than (>) number 2.
+print (result2)
