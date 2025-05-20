ad51b72ef86bc7688aec390945d996f6fbd02e1d
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-20 11:14:53 +0530

    Add twoNumbersEqual.py to compare two numbers for equality

---
+++ 19-05-2025/Comparison Operators/twoNumbersEqual.py
@@ -0,0 +1,7 @@
+number1 = int (input("enter 1st number: "))
+number2 = int (input("enter 2nd number: "))
+
+if number1 == number2:
+    print("both numbers are equal ✅")
+else:
+    print("both numbers are not equal ❌")
