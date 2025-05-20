f3c3b74574b33dbd4d1a424b358f3bea3fee62d6
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-20 10:48:47 +0530

    Add real-lifeCalculator.py to demonstrate various assignment operations

---
+++ 19-05-2025/Assignment Operators/real-lifeCalculator.py
@@ -0,0 +1,14 @@
+money = 7
+print (money)
+money += 6
+print (money)
+money -= 5
+print (money)
+money *= 4
+print (money)
+money **= 3
+print (money)
+money /= 2
+print (money)
+money //= 1
+print (money)
