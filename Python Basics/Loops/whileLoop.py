d9cc0c0d4ded2efad2065d08a55919a4dc612209
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-20 17:34:38 +0530

    Add whileLoop.py to demonstrate basic while loop functionality

---
+++ Python Basics/Loops/whileLoop.py
@@ -0,0 +1,12 @@
+# s = int(input("enter start: ")) 
+# r = int(input("enter the range: "))
+# p = input("enter what to print: ")
+# while s <= r :
+#     print(p)
+#     p += 1
+
+
+x = 1
+while x <= 100:
+    print(x)
+    x += 1
